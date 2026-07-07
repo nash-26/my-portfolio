8810782bc99b816de4342c2279520bbc11e5b193
Jonas Angeles <jonas26angeles@gmail.com>
2026-07-07 16:39:46 +0800

    changed project page

--- components/Pageprojects.tsx
+++ components/Pageprojects.tsx
@@ -2,8 +2,8 @@ import {ProjectList} from "@/components/ProjectList";
 
 export function Projectspage(){
     return(
-        <section className="min-h-screen scroll-mt-16 border-t" id="projects">
-          <h2 className="p-4 text-2xl">Projects Content</h2>
+        <section className="min-h-screen scroll-mt-16 " id="projects">
+          <h2 className="pl-4 text-2xl">My School Projects</h2>
           <div className="m-6">
             <ProjectList/>
           </div>
